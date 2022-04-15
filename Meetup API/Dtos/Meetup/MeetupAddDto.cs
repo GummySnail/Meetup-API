66fd9b6985383a0ed55acf2346d9fca69eceeac8
Vlad <vlad.mikhalchuk.01@mail.ru>
2022-04-15 13:12:23 +0300

    Adding Dtos

---
+++ Meetup API/Dtos/Meetup/MeetupAddDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Meetup_API.Dtos.Meetup;
+
+public class MeetupAddDto
+{
+    [Required]
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string City { get; set; }
+    public string Street { get; set; }
+    public int HomeNumber { get; set; }
+    public DateTimeOffset StartMeetupDateTime { get; set; }
+    public DateTimeOffset EndMeetupDateTime { get; set; }
+    [Required]
+    public string OwnerName { get; set; } // OwnerId
+    [Required]
+    public string Tag { get; set; }
+}
---
+++ Meetup API/Dtos/Meetup/MeetupDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Meetup_API.Dtos.Meetup;
+
+public class MeetupDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string City { get; set; }
+    public string Street { get; set; }
+    public int HomeNumber { get; set; }
+    public DateTimeOffset StartMeetupDateTime { get; set; }
+    public DateTimeOffset EndMeetupDateTime { get; set; }
+    public string OwnerName { get; set; } // OwnerId
+    public string Tag { get; set; } // Tags Collection<>
+}
